136a4790e940ca15994003de234d2cbbfe2c6cdd
jph <jph139883@alibaba-inc.com>
2018-08-23 20:39:12 +0800

    update example

---
+++ examples/App.js
@@ -0,0 +1,62 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+import CheckBox from 'react-native-check-box'
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+type Props = {};
+export default class App extends Component<Props> {
+  constructor(props){
+    super(props);
+    this.state={
+      isChecked:true
+    }
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <CheckBox
+            style={{flex: 1, padding: 10}}
+            onClick={()=>{
+              this.setState({
+                  isChecked:!this.state.isChecked
+              })
+            }}
+            isChecked={this.state.isChecked}
+            leftText={"CheckBox"}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
