24bd62f8a27e49146680973573a09805758d135f
mfik <marcofikkers@gmail.com>
2023-04-13 14:42:51 +0200

    Update index.js
    Deleted Deprecated ViewPropTypes

--- index.js
+++ index.js
@@ -13,9 +13,11 @@ import {
     View,
     Image,
     Text,
-    TouchableHighlight,
-    ViewPropTypes as RNViewPropTypes,
+    TouchableHighlight
 } from 'react-native';
+
+import {ViewPropTypes as RNViewPropTypes} from 'deprecated-react-native-prop-types';
+
 import PropTypes from 'prop-types';
 
 const ViewPropTypes = RNViewPropTypes || View.propTypes;
